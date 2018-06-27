7fc3de61b37cf3248c64a525532be667ff9e8563
Fred <fredadewole@gmail.com>
2018-06-27 13:49:44 +0100

    feature(Free MeetingRooms): get available rooms from Gsuite

--- app/src/main/java/com/andela/mrm/room_booking/room_availability/views/RoomAvailabilityActivity.java
+++ app/src/main/java/com/andela/mrm/room_booking/room_availability/views/RoomAvailabilityActivity.java
@@ -5,21 +5,19 @@ import android.accounts.AccountManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import com.andela.mrm.R;
 import com.andela.mrm.contract.IGoogleCalenderCallListener;
-import com.andela.mrm.presenter.GsuitePresenter;
 import com.andela.mrm.presenter.MakeGoogleCalendarCallPresenter;
 import com.andela.mrm.room_information.RoomInformationActivity;
 import com.andela.mrm.util.GooglePlayService;
@@ -199,7 +197,7 @@ public class RoomAvailabilityActivity extends AppCompatActivity implements
                     .show();
         } else {
             new MakeGoogleCalendarCallPresenter(mCredential, this).execute();
-            new GsuitePresenter(mCredential).execute();
+//            new GsuitePresenter(mCredential, null).execute();
         }
     }
 
