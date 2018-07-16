ede06336dd96a4cf1363c4d3d6b3f234adb73ddb
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2018-07-16 18:10:16 +0100

    add a wrapper class for working with the Calendar util DateTime objects

--- app/src/main/java/com/andela/mrm/util/DateTimeUtils.java
+++ app/src/main/java/com/andela/mrm/util/DateTimeUtils.java
@@ -1,33 +1,69 @@
 package com.andela.mrm.util;
 
+import com.google.api.client.util.DateTime;
+
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.TimeZone;
 
-public class DateTime {
+/**
+ * Util functions for working with {@link DateTime}.
+ */
+public final class DateTimeUtils {
     /**
-     * .
-     *
-     * @return w
+     * Instantiates a new Date time utils.
      */
-    public static com.google.api.client.util.DateTime getTime(int hour, int minute, int seconds) {
-        java.util.Calendar calendar = new GregorianCalendar();
-
-        // TODO: remove the month and day; this should use the current day
-        calendar.set(Calendar.MONTH, 6);
-        calendar.set(Calendar.DAY_OF_MONTH, 13);
-
-        calendar.set(java.util.Calendar.HOUR_OF_DAY, hour);
-        calendar.set(java.util.Calendar.MINUTE, minute);
-        calendar.set(java.util.Calendar.SECOND, seconds);
-
-        // TODO: probably pass in the timezone dynamically;
-        calendar.setTimeZone(TimeZone.getDefault());
-
-        return new com.google.api.client.util.DateTime(calendar.getTime());
+    private DateTimeUtils() {
+        // Utility class
     }
 
-    public static int getDuration(long start, long end) {
-        return (int) ((end - start) / 60000);
+    /**
+     * Creates a new {@link DateTime} with the set of params passed in.
+     *
+     * @param hour    the hour
+     * @param minute  the minute
+     * @param seconds the seconds
+     * @return {@link DateTime}
+     */
+    public static DateTime getTime(int hour, int minute, int seconds) {
+        Calendar calendar = new GregorianCalendar();
+
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, seconds);
+
+        return new DateTime(calendar.getTime());
+    }
+
+
+    /**
+     * Computes the duration from {@link DateTime} objects.
+     *
+     * @param start the start
+     * @param end   the end
+     * @return the duration in minutes
+     */
+    public static int getDuration(DateTime start, DateTime end) {
+        return (int) Math.abs((end.getValue() - start.getValue()) / 60000);
+    }
+
+    /**
+     * Gets the hour for a {@link DateTime}.
+     *
+     * @param time the time
+     * @return the hour
+     */
+    public static int getHour(DateTime time) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(time.getValue());
+        return calendar.get(Calendar.HOUR_OF_DAY);
+    }
+
+    /**
+     * Gets current time.
+     *
+     * @return the current time
+     */
+    public static DateTime getCurrentTime() {
+        return new DateTime(System.currentTimeMillis());
     }
 }
