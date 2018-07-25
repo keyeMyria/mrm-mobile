c238aab2a167e2d94514b539995175afefa4ff3f
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-07-25 14:55:58 +0300

    [ch #159165032] Add comments to script

--- .circleci/get_artifact_path.py
+++ .circleci/get_artifact_path.py
@@ -1,23 +1,23 @@
 import re
 
 
+# read artifact_url file
 def read_url_file():
-    print "read url file ============"
     url_file = open("artifact_url", "r")
     if url_file.mode == "r":
         return url_file.read()
 
 
+# extract path and store in artifact_path file
 def create_path_file():
     url = read_url_file()
-    print url, "+++++++++++++++++++++"
+    print "============= url: ", url, "=================="
     file_path = re.sub(r"(?<=)(.*)(?=test)", "", url)
-    print file_path, "******************"
+    print "============= path: ", file_path, "=============="
     path_file = open("artifact_path", "w+")
     if path_file.mode == "w+":
         path_file.write(file_path)
         path_file.close()
-    print "DONE#####################"
 
 
 if __name__ == "__main__":
